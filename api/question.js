ea52a990f72c1823c4c40fd22ea883986a4501c4
Marco Aurélio Prado <33600883+marco-prado@users.noreply.github.com>
2025-05-25 16:36:19 -0400

    fix: teeth dict

--- api/question.js
+++ api/question.js
@@ -2,7 +2,38 @@ import path from 'path';
 import fs from 'fs';
 
 const teeth = {
-    "11": "Incisivo Central Superior Direito"
+    "11": "Incisivo Central Superior Direito",
+    "12": "Incisivo Lateral Superior Direito",
+    "13": "Canino Superior Direito",
+    "14": "Primeiro Pré-Molar Superior Direito",
+    "15": "Segundo Pré-Molar Superior Direito",
+    "16": "Primeiro Molar Superior Direito",
+    "17": "Segundo Molar Superior Direito",
+    "18": "Terceiro Molar Superior Direito",
+    "21": "Incisivo Central Superior Esquerdo",
+    "22": "Incisivo Lateral Superior Esquerdo",
+    "23": "Canino Superior Esquerdo",
+    "24": "Primeiro Pré-Molar Superior Esquerdo",
+    "25": "Segundo Pré-Molar Superior Esquerdo",
+    "26": "Primeiro Molar Superior Esquerdo",
+    "27": "Segundo Molar Superior Esquerdo",
+    "28": "Terceiro Molar Superior Esquerdo",
+    "41": "Incisivo Central Inferior Direito",
+    "42": "Incisivo Lateral Inferior Direito",
+    "43": "Canino Inferior Direito",
+    "44": "Primeiro Pré-Molar Inferior Direito",
+    "45": "Segundo Pré-Molar Inferior Direito",
+    "46": "Primeiro Molar Inferior Direito",
+    "47": "Segundo Molar Inferior Direito",
+    "48": "Terceiro Molar Inferior Direito",
+    "31": "Incisivo Central Inferior Esquerdo",
+    "32": "Incisivo Lateral Inferior Esquerdo",
+    "33": "Canino Inferior Esquerdo",
+    "34": "Primeiro Pré-Molar Inferior Esquerdo",
+    "35": "Segundo Pré-Molar Inferior Esquerdo",
+    "36": "Primeiro Molar Inferior Esquerdo",
+    "37": "Segundo Molar Inferior Esquerdo",
+    "38": "Terceiro Molar Inferior Esquerdo",
 };
 
 
